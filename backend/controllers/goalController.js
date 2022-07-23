cdd8022dc832ac24320db713243d7c0cd121090a
miftah24 <miftahuddin241199@gmail.com>
2022-07-23 16:45:57 +0700

    commit

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,7 +1,35 @@
+
+// @desc    Get goals
+// @route   GET /api/goals
+// @access  Private
 const getGoals = (req,res)=>{
-    res.status(200).json({message: 'Get Goals'})
+    res.status(200).json({meesage: 'Get Goals'})
+}
+
+// @desc    Set goals
+// @route   POST /api/goals
+// @access  Private
+const setGoal = (req,res)=>{
+    res.status(200).json({meesage: 'Set Goal'})
+}
+
+// @desc    Update goals
+// @route   PUT /api/goals
+// @access  Private
+const updateGoal = (req,res)=>{
+    res.status(200).json({meesage: `Update Goal ${req.params.id}`})
+}
+
+// @desc    Delete goal
+// @route   DELETE /api/goals
+// @access  Private
+const deleteGoal = (req,res)=>{
+    res.status(200).json({meesage: `Delete Goal ${req.params.id}`})
 }
 
 module.exports = {
-    getGoals
+    getGoals,
+    setGoal,
+    updateGoal,
+    deleteGoal
 }
--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -9,11 +9,11 @@ router.post('/',(req,res)=>{
 })
 
 router.put('/:id',(req,res)=>{
-    res.status(200).json({message: 'Update Goal ${req.params.id}'})
+    res.status(200).json({message: `Update Goal ${req.params.id}`})
 })
 
 router.delete('/:id',(req,res)=>{
-    res.status(200).json({message: 'Delete Goal ${req.params.id}'})
+    res.status(200).json({message: `Delete Goal ${req.params.id}`})
 })
 
 module.exports = router
