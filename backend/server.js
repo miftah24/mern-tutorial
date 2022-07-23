1363bcfbb41884d90a4caa30f94c488a6a42e9f0
miftah24 <miftahuddin241199@gmail.com>
2022-07-23 14:44:45 +0700

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
-console.log("Hello World")
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.port || 5000
+
+const app = express()
+app.get('/api/goals',(req,res)=>{
+    res.status(200).json({message: 'Get Goals'})
+})
+app.use('/api/goals',require('./routes/goalRoutes.js'))
+app.listen(port, () => console.log(`Server started on port ${port}`))
