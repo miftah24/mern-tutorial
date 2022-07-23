022129ca932f384e3e6933bd339666ac1cd1464a
miftah24 <miftahuddin241199@gmail.com>
2022-07-23 19:51:48 +0700

    commit

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -3,28 +3,32 @@
 // @route   GET /api/goals
 // @access  Private
 const getGoals = (req,res)=>{
-    res.status(200).json({meesage: 'Get Goals'})
+    res.status(200).json({message: 'Get Goals'})
 }
 
 // @desc    Set goals
 // @route   POST /api/goals
 // @access  Private
 const setGoal = (req,res)=>{
-    res.status(200).json({meesage: 'Set Goal'})
+    if(!req.body.text){
+        res.status(400)
+        throw new Error('Please add a text field')
+    }
+    res.status(200).json({message: 'Set Goal'})
 }
 
 // @desc    Update goals
 // @route   PUT /api/goals
 // @access  Private
 const updateGoal = (req,res)=>{
-    res.status(200).json({meesage: `Update Goal ${req.params.id}`})
+    res.status(200).json({message: `Update Goal ${req.params.id}`})
 }
 
 // @desc    Delete goal
 // @route   DELETE /api/goals
 // @access  Private
 const deleteGoal = (req,res)=>{
-    res.status(200).json({meesage: `Delete Goal ${req.params.id}`})
+    res.status(200).json({message: `Delete Goal ${req.params.id}`})
 }
 
 module.exports = {
