1d3435b3c4ce0f1515c3b06963df32d6ef1189bd
miftah24 <miftahuddin241199@gmail.com>
2022-07-23 15:15:02 +0700

    initial commit

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,7 +1,17 @@
 const express = require('express')
+const res = require('express/lib/response')
 const router = express.Router()
 
 router.get('/',(req,res)=>{
     res.status(200).json({message: 'Get Goals'})
+})
+router.post('/',(req,res)=>{
+    res.status(200).json({message: 'Set Goal'})
+})
+router.put('/:id',(req,res)=>{
+    res.status(200).json({message: 'Update Goal ${req.params.id}'})
+})
+router.delete('/:id',(req,res)=>{
+    res.status(200).json({message: 'Delete Goal ${req.params.id}'})
 })
 module.exports = router
